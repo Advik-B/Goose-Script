8cae2bc5bc736c8db8736c07681cb0e28fead873
Advik <advik.b@gmail.com>
2021-12-13 15:10:42 +0000

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -13,3 +13,19 @@ code = []
 with open(file_path, 'r') as f:
     for line in f.readlines():
         code.append(line)
+
+code2 = []
+for line in code:
+    raw = line.replace(' ', '').replace('	', '')
+    if raw.startswith('#') and not raw.startswith('#*'):
+        continue
+    if raw.startswith('\n'):
+        continue
+
+    code2.append(line)
+
+code.clear()
+code.extend(code2)
+code2.clear()
+
+print(*code, sep='\n')
