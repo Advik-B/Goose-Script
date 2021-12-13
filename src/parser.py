c9bf578abe48da2a853f00f1081423ec91ba6e5a
Advik <advik.b@gmail.com>
2021-12-13 14:54:15 +0000

    HELP HOW DO I PARSE ARGS

--- src/parser.py
+++ src/parser.py
@@ -1,7 +1,6 @@
-from functions import *
-from keys import *
 import sys
 import functions
+import keys
 
 if len(sys.argv) == 1:
     print("Usage: ./parser.py <path>")
@@ -14,25 +13,3 @@ code = []
 with open(file_path, 'r') as f:
     for line in f.readlines():
         code.append(line)
-
-#print(*code, sep="\n")
-
-code_ = []
-
-for line in code:
-    tmp = line.replace(" ", "")
-    if tmp.startswith("#"):
-        del tmp
-        continue
-    line_ = []
-    for letter in line:
-        if letter == ' ':
-            letter = ''
-        line_.append(letter)
-    code_.append(line_)
-
-del line_
-code = code_
-del code_
-
-print(*code, sep="\n")
