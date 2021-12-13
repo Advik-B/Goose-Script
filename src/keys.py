221a46faf5527aa90a60d45680afba751dc4e4fa
Advik <advik.b@gmail.com>
2021-12-13 12:58:35 +0000

    Update keys.py
    Add extra keys

--- src/keys.py
+++ src/keys.py
@@ -2,3 +2,49 @@ import keyboard
 
 def ESC():
     keyboard.press_and_release('esc')
+
+def ENTER():
+    keyboard.press_and_release('enter')
+
+def TAB():
+    keyboard.press_and_release('tab')
+
+def CTRL():
+    keyboard.press_and_release('ctrl')
+    
+def SHIFT():
+    keyboard.press_and_release('shift')
+
+def SUPER():
+    keyboard.press_and_release('win')
+
+def SPACE():
+    keyboard.press_and_release('space')
+
+def PAGEUP():
+    keyboard.press_and_release('pageup')
+
+def PAGEDOWN():
+    keyboard.press_and_release('pagedown')
+    
+def END():
+    keyboard.press_and_release('end')
+
+def HOME():
+    keyboard.press_and_release('home')
+
+def LEFT():
+    keyboard.press_and_release('left')
+
+def RIGHT():
+    keyboard.press_and_release('right')
+
+def UP():
+    keyboard.press_and_release('up')
+
+def DOWN():
+    keyboard.press_and_release('down')
+
+def NUM_LOCK():
+    keyboard.press_and_release('numlock')
+
