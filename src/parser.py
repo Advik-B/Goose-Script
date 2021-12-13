995a15a959097ba48e4f021764b3f61af7463fe2
Advik <advik.b@gmail.com>
2021-12-13 13:51:17 +0000

    Update parser.py

--- src/parser.py
+++ src/parser.py
@@ -1,2 +1,26 @@
 from functions import *
 from keys import *
+import sys
+
+if len(sys.argv) == 1:
+    print("Usage: ./parser.py <path>")
+    sys.exit(1)
+
+file_path = sys.argv[1]
+
+code = []
+
+with open(file_path, 'r') as f:
+    for line in f.readlines():
+        code.append(line)
+
+#print(*code, sep="\n")
+
+for line in code:
+    tmp = line.replace(" ", "")
+    if tmp.startswith("#"):
+        del tmp
+        continue
+    if line.startswith('LOG'):
+        print(line.split('(')[1].split(')')[0].replace("'", "").replace('"', ''))
+        
